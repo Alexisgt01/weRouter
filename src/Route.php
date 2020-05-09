af0ca669cf728656020cca40e1afac17032b0b69
Alexis <alexis.gatuingt@gmail.com>
2020-05-09 16:09:41 +0200

    router factory, regex matching, http get method basic

--- src/Route.php
+++ src/Route.php
@@ -4,5 +4,114 @@ namespace WeRouter;
 
 class Route
 {
+    /**
+     * Uri asked
+     * @var string
+     */
+    public $uri;
+
+    /**
+     * Action returned
+     * @var string | callable
+     */
+    private $callable;
+
+    /**
+     * Parameters
+     * @var array
+     */
+    private $params = [];
+
+    /**
+     * RegEx matching  parameters
+     * @var string
+     */
+    private $regex = "/(?<=\{).+?(?=\})/";
+
+    /**
+     * RegEx replace by empty
+     * @var string
+     */
+    private $regex_replace = "/[{}]/";
+
+    /**
+     * Route constructor.
+     * @param $uri string
+     * @param $callable string | callable
+     */
+    public function __construct($uri, $callable)
+    {
+        $this->uri      = trim($uri, '/');
+        $this->callable = $callable;
+    }
+
+    /**
+     * Dispatch callable
+     * @param string $namespace
+     */
+    public function dispatch($namespace)
+    {
+        if (is_callable($this->callable)) {
+            $func = $this->callable;
+            !empty($this->params) ? $func($this->params) : $func();
+        } else {
+            $this->callController($namespace);
+        }
+    }
+
+    /**
+     * Matching statement
+     * @param string $request
+     * @return bool
+     */
+    public function matched($request)
+    {
+        if ($p = $this->hasParams($request)) {
+            return $p;
+        } else {
+            return $this->uri === $request;
+        }
+    }
+
+    /**
+     * Check if request has params
+     * @param string $request
+     * @return bool
+     */
+    private function hasParams($request)
+    {
+        $uri      = explode('/', $this->uri);
+        $request_ = explode('/', $request);
+        preg_match_all($this->regex, $this->uri, $uri_);
+        return count($uri_[0]) > 0 && count($uri) === count($request_) ? $this->setParams($uri, $request_) : false;
+    }
+
+    /**
+     * @param array $uri
+     * @param array $request
+     * @return bool
+     */
+    private function setParams($uri, $request)
+    {
+        foreach ($uri as $k => $v):
+            if (preg_match($this->regex, $v)) {
+                $this->params[preg_replace($this->regex_replace, '', $v)] = $request[$k];
+            }
+        endforeach;
+        return true;
+    }
+
+    /**
+     * Call controller
+     * @param string $namespace
+     */
+    private function callController($namespace)
+    {
+        $name = $namespace . explode('@', $this->callable)[0];
+        $func = explode('@', $this->callable)[1];
+        $c    = new $name();
+        !empty($this->params) ? $c->$func($this->params) : $c->$func();
+    }
+
 
 }
--- src/Router.php
+++ src/Router.php
@@ -4,5 +4,64 @@ namespace WeRouter;
 
 class Router
 {
+    /**
+     * All instance of Route
+     * @var array
+     */
+    public static $routes = [];
+
+    /**
+     * REQUEST_URI
+     * @var string
+     */
+    public static $request;
+
+    /**
+     * Controller namespace
+     * @var string
+     */
+    public static $namespace;
+
+    /**
+     * Instanced Route
+     * @param string $uri
+     * @param string|callable $callable
+     * @return Route
+     */
+    public static function get($uri, $callable)
+    {
+        $route          = new Route($uri, $callable);
+        self::$routes[] = $route;
+        self::$request  = trim($_SERVER['REQUEST_URI'], '/');
+        return $route;
+    }
+
+    public static function setNamespace($namespace)
+    {
+        self::$namespace = $namespace;
+    }
+
+    /**
+     * Run Router
+     */
+    public static function run()
+    {
+        self::match();
+    }
+
+    /**
+     * Match instance with request
+     */
+    private static function match()
+    {
+        foreach (self::$routes as $route) {
+            if ($route->matched(self::$request)) {
+                return $route->dispatch(self::$namespace);
+            }
+        }
+        // THROW
+        http_response_code(404);
+        exit;
+    }
 
 }
